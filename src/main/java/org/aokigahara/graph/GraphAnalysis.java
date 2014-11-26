b19ad2034ae71501f996248a3250d4550f484470
rieko <rieko@aokigahara.org>
2014-11-25 19:13:19 -0700

    init

---
+++ src/main/java/org/aokigahara/graph/GraphAnalysis.java
@@ -0,0 +1,21 @@
+package org.aokigahara.graph;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.aokigahara.commons.HashList;
+import org.jgrapht.Graph;
+
+/**
+ * TODO
+ * 
+ * @author rieko
+ * 
+ */
+public final class GraphAnalysis {
+	public static <V, E> List<V> hamiltonianCycle(Graph<V, E> g) {
+		List<V> tour = new HashList<V>(new LinkedList<V>());
+		// TODO
+		return tour;
+	}
+}
---
+++ src/main/java/org/aokigahara/graph/calculator/ffxiii_2/AnomalyClockCalculator.java
@@ -0,0 +1,24 @@
+package org.aokigahara.graph.calculator.ffxiii_2;
+
+
+/**
+ * TODO
+ * 
+ * @author rieko
+ */
+public class AnomalyClockCalculator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println(args);
+		return;
+		// DefaultDirectedGraph<Integer, DefaultEdge> g = new
+		// DefaultDirectedGraph<Integer, DefaultEdge>(
+		// DefaultEdge.class);
+		// // TODO
+		// List hamCycle = GraphAnalysis.hamiltonianCycle(g);
+		// System.out.println(hamCycle);
+	}
+}
